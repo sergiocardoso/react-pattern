edf06f9e0aae16a55b760f7ca8ca21aed6f284dc
sergiocardoso <contato@sergiocardoso.org>
2021-03-07 22:37:30 -0300

    chore: add

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   roots: ["<rootDir>/src"],
   collectCoverageFrom: ["<rootDir>/src/**/*.{ts,tsx}"],
   coverageDirectory: "coverage",
-  testEnviroment: "node",
+  testEnvironment: "node  
+  ",
   transform: {
     ".+\\.ts$": "ts-jest",
   },
--- src/a.spec.ts
+++ src/a.spec.ts
@@ -2,4 +2,5 @@ describe('aa', () => {
   test('aa', () => {
     expect(1).toBe(1)
   })
+  test("ola mundo");
 })
